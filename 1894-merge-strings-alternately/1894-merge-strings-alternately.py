56eedb3b4f0105db6d604518524917581d7f8374
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2025-07-15 00:29:12 +0500

    Time: 37 ms (64.14%), Space: 17.9 MB (33.5%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,8 +5,6 @@ class Solution:
         while index < len(word1) and index < len(word2):
             s += word1[index] + word2[index]
             index += 1
-        if len(word1) < len(word2):
-            s += word2[index:]
-        elif len(word2) < len(word1):
-            s += word1[index:]
+        s += word2[index:]
+        s += word1[index:]
         return s
