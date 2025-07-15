0d1d374ce5794dfe86d08166b2d30af974f440c0
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2025-07-15 10:35:59 +0500

    Time: 3 ms (52.59%), Space: 18 MB (24.5%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s = sorted(s)  
-        t = sorted(t)
-        r = 0
-        for i, c in enumerate(s):
-            r = r ^ ord(c)
-            r = r ^ ord(t[i])
-        r = r ^ ord(t[-1])
-        return chr(r)
+        for i in set(t):
+            if s.count(i)!=t.count(i):
+                return i
