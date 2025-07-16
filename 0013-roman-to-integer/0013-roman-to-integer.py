5a006641aff1635bb66de20a65707e45da9fa965
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2025-07-16 23:05:05 +0500

    Time: 11 ms (15.14%), Space: 18 MB (20.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,26 +1,20 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        hash = {
+        nums = {
             'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500,
-            'M':1000
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000,
         }
-        num = 0
-        flag = False
+        num = nums[s[0]]
         for i, char in enumerate(s):
-            if flag:
-                flag = False
-                continue
             if i+1 < len(s):
-                if (char == 'I' and (s[i+1] == 'V' or s[i+1] == 'X')) or(char == 'X' and (s[i+1] == 'L' or s[i+1] == 'C')) or (char == 'C' and (s[i+1] == 'D' or s[i+1] == 'M')):
-                    num += hash[s[i+1]] - hash[char]
-                    flag = True
+                if nums[char] >= nums[s[i+1]]:
+                    num += nums[s[i+1]]
                 else:
-                    num += hash[char]
-            else:
-                num += hash[char]
+                    num += nums[s[i+1]] - (nums[char]*2)
         return num
+                
